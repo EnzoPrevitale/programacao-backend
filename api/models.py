cbd90a1550648d8d6331dfa5e9dc0104a258ecd1
EnzoPrevitale <enzogabrielprevitale@gmail.com>
2025-08-07 16:32:23 -0300

    Atualização no banco

--- api/models.py
+++ api/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class Author(models.Model):
-    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=255)
     surname = models.CharField(max_length=255)
     birth_date = models.DateField(null=True, blank=True)
--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from views import *
+from .views import *
 
 urlpatterns = [
     path("authors/", AuthorsView.as_view())
